8a30a5e83637478f8139e1b8c260576ab27d5ada
George Alexandru Ailenei <george.ailenei@AC-99.local>
2024-02-23 11:11:05 +0200

    GraphQL and query

--- src/api/main.tsx
+++ src/api/main.tsx
@@ -1,28 +1,6 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const client = new ApolloClient({
+export const client = new ApolloClient({
   uri: "https://flyby-router-demo.herokuapp.com/",
   cache: new InMemoryCache(),
 });
-
-// const client = ...
-
-client
-  .query({
-    query: gql`
-      query GetLocations {
-        locations {
-          id
-          name
-          description
-          photo
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "@/styles/globals";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "@/api/main";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </>
   );
 }
