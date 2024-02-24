dd69f20d03ab5576e30576dd840e5ad2864ee3f1
George Alexandru Ailenei <george.ailenei@AC-99.local>
2024-02-24 14:05:27 +0200

    fetching data with apollo - commented

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,51 +2,52 @@ import { Header, Navigation, Footer } from "@/components";
 import { useQuery, gql } from "@apollo/client";
 
 export default function Home() {
-  const GET_LOCATIONS = gql`
-    query GetLocations {
-      locations {
-        id
-        name
-        description
-        photo
-      }
-    }
-  `;
+  // const GET_LOCATIONS = gql`
+  //   query GetLocations {
+  //     locations {
+  //       id
+  //       name
+  //       description
+  //       photo
+  //     }
+  //   }
+  // `;
 
-  function DisplayLocations() {
-    const { loading, error, data } = useQuery(GET_LOCATIONS);
+  // function DisplayLocations() {
+  //   const { loading, error, data } = useQuery(GET_LOCATIONS);
 
-    if (loading) return <p>Loading...</p>;
+  //   if (loading) return <p>Loading...</p>;
 
-    if (error) return <p>Error : {error.message}</p>;
+  //   if (error) return <p>Error : {error.message}</p>;
 
-    return data.locations.map(({ id, name, description, photo }) => (
-      <div key={id}>
-        <h3>{name}</h3>
+  //   return data.locations.map(
+  //     ({ id, name, description, photo }: { id: number }) => (
+  //       <div key={id}>
+  //         <h3>{name}</h3>
 
-        <img
-          width="400"
-          height="250"
-          alt="location-reference"
-          src={`${photo}`}
-        />
+  //         <img
+  //           width="400"
+  //           height="250"
+  //           alt="location-reference"
+  //           src={`${photo}`}
+  //         />
 
-        <br />
+  //         <br />
 
-        <b>About this location:</b>
+  //         <b>About this location:</b>
 
-        <p>{description}</p>
+  //         <p>{description}</p>
 
-        <br />
-      </div>
-    ));
-  }
+  //         <br />
+  //       </div>
+  //     )
+  //   );
+  // }
 
   return (
     <>
       <Navigation />
       <Header />
-      {/* {DisplayLocations()} */}
       <DisplayLocations />
       <Footer />
     </>
