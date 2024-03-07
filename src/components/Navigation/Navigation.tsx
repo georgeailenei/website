e6de319dedf484c78266199af2cd10f3e3e1dda0
George Alexandru Ailenei <george.ailenei@AC-99.local>
2024-03-07 17:26:48 +0200

    navigation reponsive

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import BurgerMenu from "../../../public/navigation/burgerMenu.svg";
 
 import {
   StyledContainer,
@@ -7,6 +9,7 @@ import {
   StyledLogoWrapper,
   StyledNav,
   StyledButtonsWrapper,
+  StyledLangWrapper,
 } from "./Styled.Navigation";
 
 export default function Navigation() {
@@ -19,16 +22,19 @@ export default function Navigation() {
       </StyledLogoWrapper>
 
       <StyledNav>
-        <Link href="/">Home</Link>
-        <Link href="/community">Community</Link>
-        <Link href="/learn">Learn</Link>
-        <Link href="/report">Report</Link>
         <Link href="/contact">Contact</Link>
+        <Link href="/report">Report</Link>
+        <Link href="/learn">Learn</Link>
+        <Link href="/community">Community</Link>
+        <Link href="/">Home</Link>
       </StyledNav>
 
       <StyledButtonsWrapper>
-        <button>EN</button>
-        <button>RO</button>
+        <Image src={BurgerMenu} alt="Burger Menu" width={32} height={32} />
+        <StyledLangWrapper>
+          <button>EN</button>
+          <button>RO</button>
+        </StyledLangWrapper>
       </StyledButtonsWrapper>
     </StyledContainer>
   );
--- src/components/Navigation/Styled.Navigation.ts
+++ src/components/Navigation/Styled.Navigation.ts
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 
 export const StyledContainer = styled.div`
-  display: grid;
-  grid-template-columns: 15% 70% 15%;
+  display: flex;
   min-height: 48px;
   border-bottom: 0.5px solid var(--text-secondary-color);
+
+  @media (min-width: 775px) {
+    display: grid;
+    grid-template-columns: 15% 70% 15%;
+  }
 `;
 
 export const StyledLogo = styled.h1`
@@ -15,19 +19,39 @@ export const StyledLogo = styled.h1`
 
 export const StyledLogoWrapper = styled.div`
   margin: auto auto;
+  margin-left: 16px;
+
+  @media (min-width: 1024px) {
+    margin-left: 0;
+  }
 `;
 
 export const StyledNav = styled.nav`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 32px;
+  display: none;
+
+  @media (min-width: 775px) {
+    display: flex;
+    align-items: center;
+    flex-direction: row-reverse;
+    gap: 32px;
+  }
+  @media (min-width: 1024px) {
+    justify-content: center;
+  }
 `;
 
 export const StyledButtonsWrapper = styled.div`
   display: flex;
+  align-items: center;
   flex-direction: row-reverse;
-  gap: 16px;
+
+  img {
+    margin-right: 16px;
+
+    @media (min-width: 775px) {
+      display: none;
+    }
+  }
 
   button {
     all: unset;
@@ -38,5 +62,19 @@ export const StyledButtonsWrapper = styled.div`
 
   button:hover {
     color: var(--text-color);
+  }
+`;
+
+export const StyledLangWrapper = styled.div`
+  display: none;
+
+  @media (min-width: 775px) {
+    display: flex;
+    gap: 16px;
+    margin-right: 32px;
+  }
+
+  @media (min-width: 1024px) {
+    margin-right: 0px;
   }
 `;
