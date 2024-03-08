49c9406c65aee884d5472b706aa562b1293c7243
George Alexandru Ailenei <george.ailenei@AC-99.local>
2024-03-08 15:08:37 +0200

    tailwindcss & reactui unsintalled

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,12 @@
 import type { AppProps } from "next/app";
 import { GlobalStyles } from "@/styles/globals";
 import RootLayout from "@/templates/RootLayout";
-import { NextUIProvider } from "@nextui-org/react";
-import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
   return (
     <RootLayout>
-      <NextUIProvider navigate={router.push}>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </NextUIProvider>
+      <GlobalStyles />
+      <Component {...pageProps} />
     </RootLayout>
   );
 }
