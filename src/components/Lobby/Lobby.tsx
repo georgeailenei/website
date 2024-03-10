2dfbf9abaaf0985f25ecf2c88075bb7053dcb515
George Alexandru Ailenei <george.ailenei@AC-99.local>
2024-03-10 21:02:01 +0200

    lobby done

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -10,34 +10,40 @@ import {
 export default function Lobby() {
   return (
     <StyledTable>
-      <StyledTr>
-        <StyledTh>Players</StyledTh>
-        <StyledTh>Mode</StyledTh>
-        <StyledTh>Status</StyledTh>
-      </StyledTr>
-      <StyledTr>
-        <StyledTd>Maria_Anders</StyledTd>
-        <StyledTd>6-Players</StyledTd>
-        <StyledTd>Available</StyledTd>
-      </StyledTr>
-      <StyledTr>
-        <StyledTd>Maria_Anders</StyledTd>
-        <StyledTd>6-Players</StyledTd>
-        <StyledTd>Available</StyledTd>
-      </StyledTr>
-      <StyledTr>
-        <StyledTd>Maria_Anders</StyledTd>
-        <StyledTd>6-Players</StyledTd>
-        <StyledTd>Available</StyledTd>
-      </StyledTr>
-      <StyledTr>
-        <StyledTd>Maria_Anders</StyledTd>
-        <StyledTd>6-Players</StyledTd>
-        <StyledTd>Available</StyledTd>
-      </StyledTr>
-      <StyledTr>
-        <StyledJoinTd colSpan={3}>Join</StyledJoinTd>
-      </StyledTr>
+      <thead>
+        <StyledTr>
+          <StyledTh>Players</StyledTh>
+          <StyledTh>Mode</StyledTh>
+          <StyledTh>Status</StyledTh>
+        </StyledTr>
+      </thead>
+      <tbody>
+        <StyledTr>
+          <StyledTd>Maria_Anders</StyledTd>
+          <StyledTd>6-Players</StyledTd>
+          <StyledTd>Available</StyledTd>
+        </StyledTr>
+        <StyledTr>
+          <StyledTd>Maria_Anders</StyledTd>
+          <StyledTd>6-Players</StyledTd>
+          <StyledTd>Available</StyledTd>
+        </StyledTr>
+        <StyledTr>
+          <StyledTd>Maria_Anders</StyledTd>
+          <StyledTd>6-Players</StyledTd>
+          <StyledTd>Available</StyledTd>
+        </StyledTr>
+        <StyledTr>
+          <StyledTd>Maria_Anders</StyledTd>
+          <StyledTd>6-Players</StyledTd>
+          <StyledTd>Available</StyledTd>
+        </StyledTr>
+      </tbody>
+      <tfoot>
+        <StyledTr>
+          <StyledJoinTd colSpan={3}>Join</StyledJoinTd>
+        </StyledTr>
+      </tfoot>
     </StyledTable>
   );
 }
